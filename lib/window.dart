49674395694b04946fdb25d992c60fee98cfe810
LeFellowHuman <highgroundgamedev@gmail.com>
2024-10-13 16:59:14 +0300

    shapocka

--- lib/window.dart
+++ lib/window.dart
@@ -43,7 +43,7 @@ class _WindowState extends State<Window> {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Image.asset(
-                      "assets/shapochka.jpg",
+                      "assets/shapocka.jpg",
                       height: 200.0,
                       width: 200.0,
                     ),
